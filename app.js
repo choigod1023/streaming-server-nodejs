a615ab0b983a6d468bd4094d3f3b331a42d0f6c7
jangjunhyeok <choigod10234@gmail.com>
2018-11-12 22:22:08 +0900

    hololuru

--- app.js
+++ app.js
@@ -15,24 +15,83 @@ app.set('views', './views');
 app.get('/', function (req, res) {
     var file = new Array();
     fs.readdir('./public/videos/hd', function (err, files) {
-        file =file+ files+",";
-    })
-    fs.readdir('./public/videos/sd', function (err, files) {
-        file =file+ files
-        console.log(file)
-        file=file.split(",");
-        res.render('main',{
-            list:file
+        res.render('main', {
+            list: files
         })
     })
 }
 )
-app.get('/videos/:asdf',function(req,res){
+app.get('/videos/:asdf', function (req, res) {
     var videos = req.params.asdf
-    res.render('video',{
-        title:videos
+    res.render('video', {
+        title: videos
     })
 })
+
+
+app.get('/videos/sdquality/:asdf', function (req, res) {
+    var videos = req.params.asdf
+    res.render('sd', {
+        title: videos
+    })
+})
+
+app.get('/showcon', function (req, res) {
+    let sd_onechu_arr = []
+    let hd_onechu_arr = []
+    fs.readdir('./public/videos/hd', function (err, files) {
+        for (let file_name of files) {
+            if (file_name.includes("showcon")) {
+                hd_onechu_arr.push(file_name)
+            }
+        }
+
+    })
+
+    fs.readdir('./public/videos/sd', function (err, files) {
+        for (let file_name of files) {
+            if (file_name.includes("showcon")) {
+                sd_onechu_arr.push(file_name)
+            }
+        }
+
+        res.render('search', {
+            hd: hd_onechu_arr,
+            sd: sd_onechu_arr
+        })
+    })
+})
+
+app.get('/onechu', function (req, res) {
+    let sd_onechu_arr = []
+    let hd_onechu_arr = []
+    fs.readdir('./public/videos/hd', function (err, files) {
+        for (let file_name of files) {
+            if (file_name.includes("izonechu")) {
+                hd_onechu_arr.push(file_name)
+            }
+        }
+
+    })
+
+    fs.readdir('./public/videos/sd', function (err, files) {
+        for (let file_name of files) {
+            if (file_name.includes("izonechu")) {
+                sd_onechu_arr.push(file_name)
+            }
+        }
+
+        res.render('search', {
+            hd: hd_onechu_arr,
+            sd: sd_onechu_arr
+        })
+    })
+})
+
+app.get('vlive', function (req, res) {
+
+})
+
 app.listen(3030, function () {
     console.log('hi');
 })
